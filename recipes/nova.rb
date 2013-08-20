50fff4407ec38930f6faa30c78a9f5e9875d8830
Geoffrey Corey <coreyg@osuosl.org>
2013-08-20 16:33:18 -0700

    Ensuring some base packages are installed

---
+++ recipes/nova.rb
@@ -0,0 +1,23 @@
+
+# Ensure a few packages are installed
+%{libvirt libvirt-client libvirt-python}.each do |pkg|
+  package pkg
+    action :install
+end
+
+# libvirtd template stuff
+#Source: https://github.com/opscode-cookbooks/nova/blob/master/recipes/libvirt.rb
+template "/etc/libvirt/libvirtd.conf" do
+  source "libvirtd.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end # Don't need to start the service, as packstack will automatically do that
+
+template "/etc/sysconfig/libvirtd" do
+  source "libvirtd.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  only_if { platform?(%w{fedora redhat centos}) }
+end # Don't need to start the service, as packstack will automatically do that
