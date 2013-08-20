e4860aa750948445ed72b6a940cd6a4705777ab6
Geoffrey Corey <coreyg@osuosl.org>
2013-08-20 10:10:28 -0700

    Initial recipe commit for packstack base file

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'osl-packstack'
+maintainer       'Geoffrey Corey'
+maintainer_email 'coreyg@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/configures a base system for RDO Packstack fro OepnStack on CentOS.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
