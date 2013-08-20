06a7eeceda1c693b18ac9774799df84d021cc4f3
Geoffrey Corey <coreyg@osuosl.org>
2013-08-20 13:25:26 -0700

    Defined the openstack release to use

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+
+# Define the the version of rdo wanted.
+default["osl-packstack"]["release"]     =   'havana'
+
+
+# TODO: Decide whether the repo url should go in here.
+
+# TODO: Setup things like ssh keys
