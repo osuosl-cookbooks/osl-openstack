6e0a848cc107239b93ef9c6bdfd8bf6a9e28efd8
Geoffrey Corey <coreyg@osuosl.org>
2013-08-21 13:18:20 -0700

    documentation typo + edit notice description

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -1,15 +1,13 @@
 # include the umbrella packastack recipe
 include_recipe "osl-packstack::default"
 
-##### NOTICE: This install ONLY the nova compute package. This does not setup any networking
+##### NOTICE: This installs ONLY the nova compute package and configures libvirt for live migration. This does not setup any networking
 
 # Ensure a few packages are installed
 %{libvirt libvirt-client libvirt-python openstack-nova-compute}.each do |pkg|
   package pkg
     action :install
 end
-
-# TODO: install the nova compute package, also yum update to get the rdo update (kernel, iproute, etc)
 
 # libvirtd template stuff
 #Source: https://github.com/opscode-cookbooks/nova/blob/master/recipes/libvirt.rb
