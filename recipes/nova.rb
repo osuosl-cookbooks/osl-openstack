db6f6ce8a6030edc862fdf1330234aeb4096d0b3
Geoffrey Corey <coreyg@osuosl.org>
2013-08-23 10:40:28 -0700

    fixed packages install typo

--- recipes/nova.rb
+++ recipes/nova.rb
@@ -4,7 +4,7 @@ include_recipe "osl-packstack::default"
 #### NOTICE: This doesn't install the nova compute package. That is only for an explicit compute node.
 
 # Install nova and supporting packages
-%{openstack-nova-api openstack-nova-cert openstack-nova-common openstack-nova-conductor openstack-nova-console openstack-nova-network openstack-nova-novncproxy openstack-nova-scheduler python-nova python-novaclient}.each do |pkg|
+%w{openstack-nova-api openstack-nova-cert openstack-nova-common openstack-nova-conductor openstack-nova-console openstack-nova-network openstack-nova-novncproxy openstack-nova-scheduler python-nova python-novaclient}.each do |pkg|
   package pkg do
     action :install
   end
