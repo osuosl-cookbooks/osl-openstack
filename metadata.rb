7cc39beb622c79f3a4c22c42a6a50b6eb9221fef
Geoffrey Corey <coreyg@osuosl.org>
2013-09-18 10:05:45 -0700

    fixing this up to be a better cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures a base system for setting up RDO Openstack
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.4'
 
+recipe "osl-packstack", "Sets up the machine to be added to the packstack cluster"
+recipe "osl-packstack::packstack", "Packstack specific configuration and setup, doesn ot setup the compute node by itself"
+recipe "osl-packstack::compute", "Sets up the machine for being a compute node, configures libvirt settings, sets upp ssh for the nova user for resizing/migration"
 
 %w{ centos }.each do |os|
     supports os
