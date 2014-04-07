82f051107862ae44cfae6cfbfd750939b80c8e41
Geoffrey Corey <coreyg@osuosl.org>
2014-04-07 10:30:23 -0700

    fix description in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures a base system for setting up RDO Openstack
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.6'
 
-recipe "osl-packstack", "Sets up the machine to be added to the packstack cluster"
-recipe "osl-packstack::packstack", "Packstack specific configuration and setup, doesn ot setup the compute node by itself"
-recipe "osl-packstack::compute", "Sets up the machine for being a compute node, configures libvirt settings, sets upp ssh for the nova user for resizing/migration"
+recipe "osl-packstack", "Sets up the machine to be added to the foreman-openstack cluster"
+recipe "osl-packstack::default", "Sets up base repo information for openstack deployment via Foreman."
+recipe "osl-packstack::compute", "Sets up the machine for being a compute node and sets upp ssh for the nova user for resizing/migration"
 
 %w{ centos }.each do |os|
     supports os
@@ -16,7 +16,3 @@ end
 
 depends "yum"
 depends "user"
-
-## TODO: setup ssh keys for root (packstack setup) and ssh keys for nova user
-
-## Also, maybe list the packages here?
