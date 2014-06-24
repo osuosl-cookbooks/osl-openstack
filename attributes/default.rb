86f660be44902622bcdaf8158528d51d75867e3c
Lance Albertson <lance@osuosl.org>
2014-06-24 16:20:11 -0700

    Remove unused attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,1 @@
-# RDO repo information
-default['osl-openstack']['secret_file'] = "/etc/chef/encrypted_data_bag_secret"
-
-# Edit this url to point to where the rd-release rpm you want is located.
-default['rdo_repo_url'] = "http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-7.noarch.rpm"
+# osl-openstack default attributes
