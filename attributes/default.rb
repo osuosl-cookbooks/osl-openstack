b3f09f699e600851d16da941245728cf332cf8f1
Lance Albertson <lance@osuosl.org>
2014-07-30 22:48:55 -0700

    Fedora attribute fixes that aren't in upstream
    Chef Inc has removed Fedora support and as a stop gap measure we'll add some
    attribute fixes in our cookbook. This is until we get the fixes upstream.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,8 @@
 # osl-openstack default attributes
+
+# Include Fedora attribute fixes that aren't in upstream
+case platform
+when 'fedora'
+  default['openstack']['compute']['platform']['dbus_service'] = 'dbus'
+  default['openstack']['db']['python_packages']['mysql'] = 'MySQL-python'
+end
