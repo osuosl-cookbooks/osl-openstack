6254c3fa8b2a8be5da95994ccfa70b7dbbbe0d13
Lance Albertson <lance@osuosl.org>
2014-08-01 15:54:18 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures osl-openstack'
 long_description 'Installs/Configures osl-openstack'
-version          '1.0.0'
+version          '1.0.1'
 
 %w{ base firewall osl-apache modules mysql openstack-block-storage
   openstack-common openstack-compute openstack-dashboard openstack-identity
