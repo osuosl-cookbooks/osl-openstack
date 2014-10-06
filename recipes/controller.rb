c8578d8f47af9f9fc4641e121938332e4c43eaac
Lance Albertson <lance@osuosl.org>
2014-10-05 22:38:06 -0700

    Enable https proxy for api endpoints
    This enables the ability to use https for the api endpoints via mod_proxy in
    apache. While it appears to be a workaround, its a much easier method to enable
    than doing it natively. This assumes that the native api endpoints are listening
    on localhost to ensure no port conflicts.

--- recipes/controller.rb
+++ recipes/controller.rb
@@ -16,9 +16,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "apache2::mod_proxy"
+include_recipe "apache2::mod_proxy_http"
+include_recipe "apache2::mod_headers"
 include_recipe "osl-apache::default"
 include_recipe "firewall::openstack"
 include_recipe "firewall::amqp"
 include_recipe "firewall::vnc"
 include_recipe "osl-openstack::_fedora"
 include_recipe "osl-openstack::novnc"
+
+# Setup https endpoints for API layer via apache
+dashboard = node['openstack']['dashboard']
+endpoints = node['openstack']['endpoints']
+
+apache_app "openstack-https-proxy" do
+  name dashboard['server_hostname'] if dashboard['server_hostname']
+  cert_file "/etc/pki/tls/certs/#{dashboard['ssl']['cert']}"
+  cert_key "/etc/pki/tls/private/#{dashboard['ssl']['key']}"
+  cert_chain "/etc/pki/tls/certs/#{dashboard['ssl']['chain']}" if dashboard['ssl']['chain']
+  ip_address endpoints['host']
+  template "https-proxy.conf.erb"
+  cookbook "osl-openstack"
+  port [ endpoints['identity-api']['port'], endpoints['identity-admin']['port'] ]
+end
