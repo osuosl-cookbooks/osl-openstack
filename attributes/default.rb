f32312b2f0b741e685b8d6b15f68bedbab61b304
Lance Albertson <lance@osuosl.org>
2015-03-17 15:46:21 -0700

    Add attribute for setting one-off ips for the firewall

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,7 @@ default['osl-openstack']['databag_prefix'] = nil
 default['osl-openstack']['vnc_bind_interface']['controller'] = "eth1"
 default['osl-openstack']['vnc_bind_interface']['compute'] = "br42"
 default['osl-openstack']['cinder']['iscsi_role'] = nil
+default['osl-openstack']['cinder']['iscsi_ips'] = []
 
 # Include Fedora attribute fixes that aren't in upstream
 case platform
