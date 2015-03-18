1e3065ccd7d9987a0573ddd75e06b06ee6a0e1a8
Lance Albertson <lance@osuosl.org>
2015-03-18 08:48:27 -0700

    Use correct attribute name

--- recipes/cinder.rb
+++ recipes/cinder.rb
@@ -19,8 +19,8 @@
 # this is required because of the fedora deps. Will be fixed once its moved into
 # a _common recipe.
 iscsi_hosts = ['127.0.0.1']
-iscsi_hosts << node['osl-openstack']['cinder']['ips'] unless
-  node['osl-openstack']['cinder']['ips'].nil?
+iscsi_hosts << node['osl-openstack']['cinder']['iscsi_ips'] unless
+  node['osl-openstack']['cinder']['iscsi_ips'].nil?
 iscsi_role = node['osl-openstack']['cinder']['iscsi_role']
 unless iscsi_role.nil? && Chef::Config[:solo]
   search(:node, "role:#{iscsi_role}") do |n|
