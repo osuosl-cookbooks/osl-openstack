6deba02908407fe2f40efc40a22ad91f92232fc8
Jordan Evans <jordane@osuosl.org>
2015-04-20 10:05:36 -0700

    gather ppc64 specific stuff under case statement

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -53,19 +53,19 @@ when 'fedora'
       version node['osl-openstack']['openpower']['kernel_version']
       action :upgrade
     end
-  end
 
-  # Turn off smt on boot (required for KVM support)
-  # NOTE: This really should be handled via an rclocal cookbook
-  cookbook_file '/etc/rc.d/rc.local' do
-    owner 'root'
-    group 'root'
-    mode 0755
-  end
-
-  # Turn off smt during runtime
-  execute 'ppc64_cpu_smt_off' do
-    command '/sbin/ppc64_cpu --smt=off'
-    not_if '/sbin/ppc64_cpu --smt | grep \'SMT is off\''
+    # Turn off smt on boot (required for KVM support)
+    # NOTE: This really should be handled via an rclocal cookbook
+    cookbook_file '/etc/rc.d/rc.local' do
+      owner 'root'
+      group 'root'
+      mode 0755
+    end
+    
+    # Turn off smt during runtime
+    execute 'ppc64_cpu_smt_off' do
+      command '/sbin/ppc64_cpu --smt=off'
+      not_if '/sbin/ppc64_cpu --smt | grep \'SMT is off\''
+    end
   end
 end
