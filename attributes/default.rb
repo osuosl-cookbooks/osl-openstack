132c5f68d683c2cd1c5addf82ac651e0cfdaf0a0
Lance Albertson <lance@osuosl.org>
2015-04-29 17:18:56 -0700

    Only set these attributes on ppc64

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,20 +25,6 @@ default['osl-openstack']['cinder']['iscsi_ips'] = []
 # Include Fedora attribute fixes that aren't in upstream
 case platform
 when 'fedora'
-  # osl-openstack cookbook attributes
-  default['yum']['fedora']['exclude'] = 'kernel* libvirt qemu* ksm ' \
-    'libcacard* perf* python-perf*'
-  default['yum']['updates']['exclude'] = 'kernel* libvirt qemu* ksm ' \
-    'libcacard* perf* python-perf*'
-  default['osl-openstack']['openpower']['yum']['repo-key'] = 'http://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl'
-  default['osl-openstack']['openpower']['yum']['uri'] =
-    'http://ftp.osuosl.org/pub/osl/repos/yum/openpower/f$releasever/ppc64'
-  default['osl-openstack']['openpower']['kernel_version'] = value_for_platform(
-    'fedora' => {
-      '~> 20.0' => '3.16.0-1.fc20.ppc64',
-      '~> 21.0' => '3.19.5-200.fc21.ppc64'
-    }
-  )
   # openstack-* cookbook attributes
   default['openstack']['compute']['platform']['dbus_service'] = 'dbus'
   default['openstack']['db']['python_packages']['mysql'] = %w(MySQL-python)
@@ -47,4 +33,21 @@ end
 case node['kernel']['machine']
 when 'ppc64'
   default['modules']['modules'] = %w(kvm_hv)
+  case platform
+  when 'fedora'
+    # osl-openstack cookbook attributes
+    default['yum']['fedora']['exclude'] = 'kernel* libvirt qemu* ksm ' \
+      'libcacard* perf* python-perf*'
+    default['yum']['updates']['exclude'] = 'kernel* libvirt qemu* ksm ' \
+      'libcacard* perf* python-perf*'
+    default['osl-openstack']['openpower']['yum']['repo-key'] = 'http://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl'
+    default['osl-openstack']['openpower']['yum']['uri'] =
+      'http://ftp.osuosl.org/pub/osl/repos/yum/openpower/f$releasever/ppc64'
+    default['osl-openstack']['openpower']['kernel_version'] = value_for_platform(
+      'fedora' => {
+        '~> 20.0' => '3.16.0-1.fc20.ppc64',
+        '~> 21.0' => '3.19.5-200.fc21.ppc64'
+      }
+    )
+  end
 end
