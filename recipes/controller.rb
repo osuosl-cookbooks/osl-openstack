19b449b8786e8406634becc308bf3303922b32c3
Lance Albertson <lance@osuosl.org>
2015-07-02 18:42:20 -0700

    Pull in magic from roles into recipes
    This mostly means putting any attributes into the recipes and including all the
    recipes.

--- recipes/controller.rb
+++ recipes/controller.rb
@@ -16,14 +16,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+vnc_bind_int = node['osl-openstack']['vnc_bind_interface']['controller']
+node.default['openstack']['endpoints']['compute-vnc-bind']['bind_interface'] =
+  vnc_bind_int
+
 include_recipe 'osl-apache::default'
 include_recipe 'firewall::openstack'
 include_recipe 'firewall::amqp'
 include_recipe 'firewall::vnc'
 include_recipe 'osl-openstack::_fedora'
 include_recipe 'osl-openstack::default'
+include_recipe 'openstack-ops-messaging::server'
+include_recipe 'openstack-identity::server'
+include_recipe 'openstack-identity::registration'
+include_recipe 'openstack-image::api'
+include_recipe 'openstack-image::registry'
+include_recipe 'openstack-image::identity_registration'
+include_recipe 'openstack-image::image_upload'
+include_recipe 'openstack-network::identity_registration'
+include_recipe 'openstack-network::openvswitch'
+include_recipe 'openstack-network::l3_agent'
+include_recipe 'openstack-network::dhcp_agent'
+include_recipe 'openstack-network::metadata_agent'
+include_recipe 'openstack-network::server'
+include_recipe 'openstack-compute::nova-setup'
+include_recipe 'openstack-compute::identity_registration'
+include_recipe 'openstack-compute::conductor'
+include_recipe 'openstack-compute::scheduler'
+include_recipe 'openstack-compute::api-ec2'
+include_recipe 'openstack-compute::api-os-compute'
+include_recipe 'openstack-compute::api-metadata'
+include_recipe 'openstack-block-storage::api'
+include_recipe 'openstack-block-storage::scheduler'
+include_recipe 'openstack-block-storage::volume'
+include_recipe 'certificate::wildcard'
+include_recipe 'openstack-compute::nova-cert'
+include_recipe 'openstack-compute::vncproxy'
 include_recipe 'osl-openstack::novnc'
-
-vnc_bind_int = node['osl-openstack']['vnc_bind_interface']['controller']
-node.default['openstack']['endpoints']['compute-vnc-bind']['bind_interface'] =
-  vnc_bind_int
+include_recipe 'openstack-dashboard::server'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: osl-openstack
 # Recipe:: default
 #
-# Copyright (C) 2014, 2015 Oregon State University
+# Copyright (C) 2014-2015 Oregon State University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+node.default['authorization']['sudo']['include_sudoers_d'] = true
+node.default['openstack']['compute']['config']['allow_same_net_traffic'] =
+  false
+node.default['openstack']['compute']['config']['ram_allocation_ratio'] = '5.0'
+node.default['openstack']['libvirt']['virt_type'] = 'kvm'
+node.default['openstack']['network']['force_dhcp_release'] = true
+node.default['openstack']['network']['multi_host'] = true
+node.default['openstack']['network']['service_type'] = 'nova'
+node.default['openstack']['dashboard']['keystone_default_role'] = '_member_'
+node.default['openstack']['dashboard']['ssl']['cert'] = 'horizon.pem'
+node.default['openstack']['dashboard']['ssl']['cert_url'] =
+  'file:///etc/pki/tls/certs/wildcard.pem'
+node.default['openstack']['dashboard']['ssl']['chain'] = 'wildcard-bundle.crt'
+node.default['openstack']['dashboard']['ssl']['key'] = 'horizon.key'
+node.default['openstack']['dashboard']['ssl']['key_url'] =
+  'file:///etc/pki/tls/private/wildcard.key'
+node.default['openstack']['developer_mode'] = false
+node.default['openstack']['release'] = 'icehouse'
+node.default['openstack']['secret']['key_path'] =
+  '/etc/chef/encrypted_data_bag_secret'
+node.default['openstack']['sysctl']['net.ipv4.conf.all.rp_filter'] = 0
+node.default['openstack']['sysctl']['net.ipv4.conf.default.rp_filter'] = 0
+node.default['openstack']['sysctl']['net.ipv4.ip_forward'] = 1
+
 case node['platform']
 when 'fedora'
   node.default['openstack']['yum']['uri'] = 'http://repos.fedorapeople.org/' \
@@ -23,6 +47,17 @@ when 'fedora'
   node.default['openstack']['yum']['repo-key'] = 'https://github.com/' \
     "redhat-openstack/rdo-release/raw/#{node['openstack']['release']}/" \
     "RPM-GPG-KEY-RDO-#{node['openstack']['release'].capitalize}"
+  node.default['openstack']['compute']['platform']['dbus_service'] = 'dbus'
+  node.default['openstack']['db']['python_packages']['mysql'] =
+    %w(MySQL-python)
+  case node['kernel']['machine']
+  when 'ppc64'
+    node.default['modules']['modules'] = %w(kvm_hv)
+    node.default['yum']['fedora']['exclude'] = 'kernel* libvirt qemu* ksm ' \
+      'libcacard* perf* python-perf*'
+    node.default['yum']['updates']['exclude'] = 'kernel* libvirt qemu* ksm ' \
+      'libcacard* perf* python-perf*'
+  end
 when 'centos'
   node.default['openstack']['yum']['uri'] = 'http://repos.fedorapeople.org/' \
     "repos/openstack/openstack-#{node['openstack']['release']}/epel-6"
@@ -50,3 +85,11 @@ if databag_prefix
       "#{databag_prefix}_#{d}"
   end
 end
+
+include_recipe 'base::ifconfig'
+include_recipe 'selinux::permissive'
+include_recipe 'openstack-common'
+include_recipe 'openstack-common::logging'
+include_recipe 'openstack-common::set_endpoints_by_interface'
+include_recipe 'openstack-common::sysctl'
+include_recipe 'openstack-common::openrc'
