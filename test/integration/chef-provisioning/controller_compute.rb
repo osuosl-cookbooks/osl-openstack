37b891296ae890c6ada70862e60c39e6a56c1ee0
Lance Albertson <lance@osuosl.org>
2015-08-31 16:39:45 -0700

    Initial kilo changes

--- test/integration/chef-provisioning/controller_compute.rb
+++ test/integration/chef-provisioning/controller_compute.rb
@@ -3,8 +3,8 @@ require 'chef/provisioning'
 # temporary workaround for a bug with chef-provisioning
 with_chef_server Chef::Config[:chef_server_url].sub('chefzero', 'http')
 
-controller_os = ENV['CONTROLLER_OS'] || 'chef/centos-6.6'
-compute_os = ENV['COMPUTE_OS'] || 'chef/centos-6.6'
+controller_os = ENV['CONTROLLER_OS'] || 'chef/centos-7.1'
+compute_os = ENV['COMPUTE_OS'] || 'chef/centos-7.1'
 
 machine_batch do
   machine 'controller' do
@@ -13,12 +13,15 @@ machine_batch do
     }
     add_machine_options vagrant_config: <<-EOF
   config.vm.network "private_network", ip: "192.168.60.10"
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+    v.cpus = 2
+  end
 EOF
     role 'openstack_vagrant'
     recipe 'osl-openstack::ops_database'
     recipe 'osl-openstack::controller'
     recipe 'openstack-integration-test::setup'
-    recipe 'scl'
     file('/etc/chef/encrypted_data_bag_secret',
          File.dirname(__FILE__) +
          '/../default/encrypted_data_bag_secret')
