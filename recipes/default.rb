75c8354ac6959bfd9e82d3bb14c0586976be6816
Lance Albertson <lance@osuosl.org>
2015-09-10 09:25:23 -0700

    Actually enable Neutron, also make a few more adjustments in the settings

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,10 +21,23 @@ node.default['apache']['contact'] = 'hostmaster@osuosl.org'
 node.default['openstack']['compute']['config']['allow_same_net_traffic'] =
   false
 node.default['openstack']['compute']['config']['ram_allocation_ratio'] = '5.0'
+node.default['openstack']['compute']['enabled_apis'] = 'ec2,osapi_compute'
 node.default['openstack']['compute']['network']['service_type'] = 'neutron'
 node.default['openstack']['compute']['network']['multi_host'] = true
 node.default['openstack']['compute']['network']['force_dhcp_release'] = true
 node.default['openstack']['libvirt']['virt_type'] = 'kvm'
+node.default['openstack']['network']['service_plugins'] =
+  [ 'neutron.services.l3_router.l3_router_plugin.L3RouterPlugin' ]
+node.default['openstack']['network']['dhcp']['enable_isolated_metadata'] =
+  'True'
+node.default['openstack']['network']['openvswitch']['tunnel_id_ranges'] =
+  '1:1000'
+node.default['openstack']['network']['openvswitch']['enable_tunneling'] =
+  'True'
+node.default['openstack']['network']['openvswitch']['tunnel_type'] = 'gre'
+node.default['openstack']['network']['openvswitch']['tenant_network_type'] =
+  'gre'
+node.default['openstack']['network']['quota']['floatingip'] = 50
 node.default['openstack']['dashboard']['keystone_default_role'] = '_member_'
 node.default['openstack']['dashboard']['ssl']['cert'] = 'horizon.pem'
 node.default['openstack']['dashboard']['ssl']['cert_url'] =
@@ -56,12 +69,12 @@ end
 case node['platform']
 when 'fedora'
   node.default['openstack']['yum']['uri'] = 'http://repos.fedorapeople.org/' \
-    "repos/openstack/EOL/openstack-#{node['openstack']['release']}/fedora-20"
+    "repos/openstack/openstack-#{node['openstack']['release']}/f21"
   node.default['openstack']['yum']['repo-key'] = 'https://github.com/' \
     "redhat-openstack/rdo-release/raw/#{node['openstack']['release']}/" \
     "RPM-GPG-KEY-RDO-#{node['openstack']['release'].capitalize}"
   node.default['openstack']['compute']['platform']['dbus_service'] = 'dbus'
-  node.default['openstack']['db']['python_packages']['mysql'] =
+  node.default['openstack']['db']['python_packages']['mariadb'] =
     %w(MySQL-python)
   case node['kernel']['machine']
   when 'ppc64'
