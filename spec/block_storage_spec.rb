ea398ecce205e3d54c4b34ddb7744ea7675aa704
Lance Albertson <lance@osuosl.org>
2016-06-20 11:09:31 -0700

    ChefSpec tests for iscsi ip addresses

--- spec/block_storage_spec.rb
+++ spec/block_storage_spec.rb
@@ -23,4 +23,21 @@ describe 'osl-openstack::block_storage' do
       expect(chef_run).to include_recipe(r)
     end
   end
+  before do
+    node.set['osl-openstack']['cinder']['iscsi_role'] = 'iscsi_role'
+    node.set['osl-openstack']['cinder']['iscsi_ips'] = %w(10.11.0.1)
+    stub_search(:node, 'role:iscsi_role')
+      .and_return([{ ipaddress: '10.10.0.1' }])
+  end
+  it 'adds iscsi nodes ipaddresses' do
+    # TODO: Add a test that actually works with the node search
+    expect(chef_run).to create_iptables_ng_rule('iscsi').with(
+      rule:
+        [
+          '--protocol tcp --source 10.11.0.1 --destination-port 3260 --jump ACCEPT',
+          '--protocol tcp --source 127.0.0.1 --destination-port 3260 --jump ACCEPT'
+        ],
+      chain: 'iscsi'
+    )
+  end
 end
