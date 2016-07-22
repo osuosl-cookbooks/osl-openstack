2c326b1d9770ed0c941555aeb7ef3808193fa31e
Lance Albertson <lance@osuosl.org>
2016-07-22 16:39:04 -0700

    Remove wrong knife.rb options

--- test/chef-config/knife.rb
+++ test/chef-config/knife.rb
@@ -2,8 +2,6 @@
 # for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
-log_level :info
-log_location STDOUT
 node_name 'fakeclient'
 client_key "#{current_dir}/fakeclient.pem"
 validation_client_name 'chef-validator'
