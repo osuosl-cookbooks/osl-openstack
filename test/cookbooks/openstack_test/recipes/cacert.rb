4a70a4c092a2dbaefa35e2a63c165a2a5d9c2778
Lance Albertson <lance@osuosl.org>
2017-01-13 14:41:11 -0800

    Set Net::HTTP to use the ca-bundle.crt on test systems
    This allows for our self signed testing cert to be verified properly by ruby.
    New code was added to the openstack-identity cookbook which required this to
    work in TK.

--- test/cookbooks/openstack_test/recipes/cacert.rb
+++ test/cookbooks/openstack_test/recipes/cacert.rb
@@ -5,3 +5,20 @@ execute 'copy self-signed ca-cert' do
   EOF
   creates '/tmp/cacert'
 end
+
+# Make sure ruby knows to use the ca-bundle certs as authority so that our self signed cert gets verified properly.
+# NOTE: This is only needed for testing, not production.
+ruby_block 'set ca_file' do
+  block do
+    require 'net/https'
+
+    Net::HTTP.class_eval do
+      alias_method :_use_ssl=, :use_ssl=
+
+      def use_ssl=(boolean)
+        self.ca_file = '/etc/ssl/certs/ca-bundle.crt'
+        self._use_ssl = boolean
+      end
+    end
+  end
+end
