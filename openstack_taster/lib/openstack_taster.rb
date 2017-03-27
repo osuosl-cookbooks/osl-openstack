6e66a3161f25a9631dfd6fdde01b40c05edfe1e0
tolvstaa <tolvstaa@osuosl.org>
2017-03-27 16:49:50 -0700

    Fix some basic problems

--- openstack_taster/lib/openstack_taster.rb
+++ openstack_taster/lib/openstack_taster.rb
@@ -3,11 +3,13 @@
 class OpenStackTaster
   INSTANCE_FLAVOR = 'm1.tiny'
 
+  attr_accessor :images, :volumes
+
   def initialize(openstack, fixed_ip)
     @openstack = openstack
     @fixed_ip = fixed_ip
-    @images = @openstack.list_images
-    @volumes = @openstack.list_volumes
+    @images = @openstack.images
+    @volumes = @openstack.volumes.all
   end
 
   def taste_all
@@ -20,13 +22,13 @@ class OpenStackTaster
     username = image.name.downcase.gsub(/[^a-z].*$/, '')
     puts "Username will be #{username}"
 
-    return
-
-    instance = @openstack.servers.create(
-      name: 'test-instance',
-      flavor_ref: INSTANCE_FLAVOR,
-      image_ref: image.id
-    )
+    puts <<-CREATE
+instance = @openstack.servers.create(
+  name: 'test-instance',
+  flavor_ref: INSTANCE_FLAVOR,
+  image_ref: image.id
+)
+    CREATE
 
     failures = @volumes.any? do |volume|
       instance_volume_fails?(instance, volume)
@@ -34,11 +36,12 @@ class OpenStackTaster
 
     # shut down instance
 
-    instance.destroy unless failures
+    puts 'instance.destroy unless failures'
   end
 
   def instance_volume_fails?(instance, volume)
     # print if volume fails to mount or detach
     # return (mount_fail? || detach_fail?)
+    false
   end
 end
