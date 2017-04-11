cae6c37603536d3676b98a5b8d9c2d5649bc0318
Lance Albertson <lance@osuosl.org>
2017-04-11 10:08:17 -0700

    Add ip ranges so testing on openstack is easier

--- test/cookbooks/openstack_test/recipes/hosts_provisioning.rb
+++ test/cookbooks/openstack_test/recipes/hosts_provisioning.rb
@@ -12,3 +12,7 @@ controller_ip = controller.empty? ? node['cloud']['public_ipv4'] : controller
 hostsfile_entry controller_ip do
   hostname 'controller.example.com'
 end
+
+node.default['firewall']['range']['osl_managed']['4'] << '192.168.56.0/22'
+node.default['firewall']['range']['memcached']['4'] << '192.168.56.0/22'
+node.default['firewall']['range']['memcached']['4'] << '140.211.168.0/24'
