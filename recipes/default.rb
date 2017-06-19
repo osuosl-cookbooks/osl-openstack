b7cf67d3fb5bb37f46fc85fb856a6d11ea04165f
Lance Albertson <lance@osuosl.org>
2017-06-19 16:06:53 -0700

    Fixes for identity recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ node.default['openstack']['release'] = 'newton'
 node.default['openstack']['secret']['key_path'] =
   '/etc/chef/encrypted_data_bag_secret'
 node.default['openstack']['misc_openrc'] = [
-  'export OS_CACERT=""'
+  'export OS_CACERT="/etc/ssl/certs/ca-bundle.crt"'
 ]
 node.default['openstack']['yum']['uri'] =
   'http://centos.osuosl.org/7.3.1611/cloud/x86_64/openstack-' + node['openstack']['release']
