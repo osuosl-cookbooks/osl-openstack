ceba8b852ef29cd8c5488fcc42123f2a90252fc3
Lance Albertson <lance@osuosl.org>
2018-05-30 09:53:55 -0700

    Remove mellanox_neo recipe
    We ended up not using this

--- test/smoke/default/mellanox_neo.rb
+++
@@ -1,6 +0,0 @@
-# # encoding: utf-8
-
-# Inspec test for recipe osl-openstack::mellanox_neo
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
