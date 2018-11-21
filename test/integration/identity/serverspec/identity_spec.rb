cf5776781e7a85b713112306050a18e1702a40c2
Lance Albertson <lance@osuosl.org>
2018-11-20 19:25:18 -0800

    Check for deprecated configuration in keystone

--- test/integration/identity/serverspec/identity_spec.rb
+++ test/integration/identity/serverspec/identity_spec.rb
@@ -39,3 +39,7 @@ end
 describe command("/opt/chef/embedded/bin/gem list -i -v '< 0.2.0' fog-openstack") do
   its(:stdout) { should match(/^true$/) }
 end
+
+describe command('grep -q deprecation /var/log/keystone/keystone.log') do
+  its(:exit_status) { should eq 1 }
+end
