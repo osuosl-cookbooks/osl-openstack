48ab4c32f2a5c4a8ae1e9f2850bf34a0dd1066f8
Lance Albertson <lance@osuosl.org>
2019-10-03 17:37:32 -0700

    Update to CentOS 7.7

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "centos_atomic_image" {
     default = "CentOS Atomic 7.1902"
 }
 variable "centos_image" {
-    default = "CentOS 7.6"
+    default = "CentOS 7.7"
 }
 variable "ssh_key_name" {
     default = "bootstrap"
