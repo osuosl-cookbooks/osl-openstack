80673b846e14a04964a3668788287275c25b647c
Lance Albertson <lance@osuosl.org>
2020-10-09 17:45:12 -0700

    Ensure we remove RDO-rocky during the upgrade

--- recipes/upgrade.rb
+++ recipes/upgrade.rb
@@ -15,7 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-yum_repository 'RDO-queens' do
+yum_repository 'RDO-rocky' do
   action :delete
 end
 
