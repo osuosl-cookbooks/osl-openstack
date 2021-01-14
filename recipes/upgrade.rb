26c8aab2aa72033ccbf3e26bbbdcc380d2ddd71b
Lance Albertson <lance@osuosl.org>
2021-01-13 16:23:23 -0800

    Cookstyle fixes

--- recipes/upgrade.rb
+++ recipes/upgrade.rb
@@ -2,7 +2,7 @@
 # Cookbook:: osl-openstack
 # Recipe:: upgrade
 #
-# Copyright:: 2017-2020, Oregon State University
+# Copyright:: 2017-2021, Oregon State University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 yum_repository 'RDO-rocky' do
-  action :delete
+  action :remove
 end
 
 include_recipe 'osl-openstack'
