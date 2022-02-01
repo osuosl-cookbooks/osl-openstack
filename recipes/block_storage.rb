8ef03445f83e225ea3e3b91cf8d71e6b3e0f4007
Lance Albertson <lance@osuosl.org>
2022-02-01 12:08:55 -0800

    Cookstyle fixes
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- recipes/block_storage.rb
+++ recipes/block_storage.rb
@@ -2,7 +2,7 @@
 # Cookbook:: osl-openstack
 # Recipe:: block_storage
 #
-# Copyright:: 2015-2021, Oregon State University
+# Copyright:: 2015-2022, Oregon State University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
