8759f19fb8d8e1aa052620d804e70603e3364846
Lance Albertson <lance@osuosl.org>
2023-11-30 16:46:08 -0800

    Remove additional unused attributes
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,7 @@
-# osl-openstack default attributes
 default['osl-openstack']['databag_item'] = 'x86'
 default['osl-openstack']['ceph']['image'] = false
 default['osl-openstack']['ceph']['compute'] = false
 default['osl-openstack']['ceph']['volume'] = false
-default['osl-openstack']['block']['rbd_store_pool'] = 'volumes'
-default['osl-openstack']['block']['rbd_ssd_pool'] = 'volumes_ssd'
-default['osl-openstack']['block']['rbd_store_user'] = 'cinder'
-default['osl-openstack']['block_backup']['rbd_store_pool'] = 'backups'
-default['osl-openstack']['block_backup']['rbd_store_user'] = 'cinder-backup'
 default['osl-openstack']['seperate_network_node'] = false
 default['osl-openstack']['node_type'] = 'compute'
-default['osl-openstack']['external_networks'] = %w(public)
 default['osl-openstack']['cluster_name'] = nil
